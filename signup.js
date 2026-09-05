1da34e9d26fcf6f949ad0b40edef1bf0ee2cebac
abhaykyada <abhaykyada34@gmail.com>
2026-09-05 20:43:11 +0530

    Update signup API endpoint to relative path

--- signup.js
+++ signup.js
@@ -25,7 +25,7 @@ form.addEventListener("submit", async (event) => {
     signupBtn.classList.add("loading");
 
     try {
-        const response = await fetch("http://localhost:3000/signup", {
+        const response = await fetch("/signup", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
