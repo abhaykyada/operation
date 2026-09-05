b050dae5764a78082206171f2b570ac096e80b4e
abhaykyada <abhaykyada34@gmail.com>
2026-09-05 20:49:01 +0530

    Change static file serving to root directory

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve frontend files
-app.use(express.static(path.join(__dirname, "frontend")));
-
+app.use(express.static(__dirname));
 /*
   PostgreSQL connection
 
